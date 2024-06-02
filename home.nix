88b85d5cef07f13c7072a37e293a34a0cff90a87
Josh <josh.e.stein@gmail.com>
2024-06-02 15:08:54 -0400

    Move zsh aliases into home aliases

--- home.nix
+++ home.nix
@@ -8,6 +8,38 @@
 
   home.stateVersion = "24.05";
 
+  home.shellAliases = {
+      cat = "bat";
+      hg = "history | grep";
+      gcmsg = "git commit -m";
+      gcm = "git commit -m";
+      gca = "git commit --amend";
+      gco = "git checkout";
+      gpu = "git pull";
+      l = "eza --long --all --git";
+      ll = "eza --long --git";
+      lt = "eza --tree";
+      md = "mkcdir";
+      nb = "npm run build";
+      ni = "npm install";
+      ns = "npm start";
+      p = "python";
+      sv = "sudoedit";
+      v = "nvim";
+
+      bc = "bc -ql";
+      cp = "cp -iv";
+      df = "df -h";
+      free = "free -h";
+      grep = "grep --color=auto";
+      mv = "mv -iv";
+      rm = "rm -iv";
+
+      "..." = "../..";
+      "...." = "../../..";
+      "....." = "../../../..";
+  };
+
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
--- zsh.nix
+++ zsh.nix
@@ -5,36 +5,5 @@
     autosuggestion = { enable = true; };
     enableCompletion = true;
     syntaxHighlighting = { enable = true; };
-    shellAliases = {
-      cat = "bat";
-      hg = "history | grep";
-      gcmsg = "git commit -m";
-      gcm = "git commit -m";
-      gca = "git commit --amend";
-      gco = "git checkout";
-      gpu = "git pull";
-      l = "eza --long --all --git";
-      ll = "eza --long --git";
-      lt = "eza --tree";
-      md = "mkcdir";
-      nb = "npm run build";
-      ni = "npm install";
-      ns = "npm start";
-      p = "python";
-      sv = "sudoedit";
-      v = "nvim";
-
-      bc = "bc -ql";
-      cp = "cp -iv";
-      df = "df -h";
-      free = "free -h";
-      grep = "grep --color=auto";
-      mv = "mv -iv";
-      rm = "rm -iv";
-
-      "..." = "../..";
-      "...." = "../../..";
-      "....." = "../../../..";
-    };
   };
 }
