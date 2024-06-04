3324feb4f0225f2895239f32634a875a9b7d6c75
Josh <josh.e.stein@gmail.com>
2024-06-04 17:22:42 -0400

    Use nix-darwin

--- flake.nix
+++ flake.nix
@@ -1,24 +1,37 @@
 {
-  description = "Home Manager configuration";
+  description = "Darwin system flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }: let
-    arch = "aarch64-darwin";
-  in {
-    defaultPackage.${arch} =
-      home-manager.defaultPackage.${arch};
-
-    homeConfigurations.joshstein = 
-      home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.${arch};
-        modules = [ ./home.nix ];
-      };
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }: {
+    darwinConfigurations."joshstein-mac" = nix-darwin.lib.darwinSystem {
+      modules = [
+        # The platform the configuration will be used on.
+        { nixpkgs.hostPlatform = "aarch64-darwin"; }
+        ./unfree.nix
+        ./darwin.nix 
+        home-manager.darwinModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+# home-manager.useUserPackages = true; # this BREAKS EVERYTHING SO SAD WHY
+          home-manager.users.joshstein = import ./home.nix;
+        }
+      ];
+      specialArgs = { inherit self; };
     };
+
+    # Expose the package set, including overlays, for convenience.
+# darwinPackages = self.darwinConfigurations."joshstein-mac".pkgs;
+  };
 }
