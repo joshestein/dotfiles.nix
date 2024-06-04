c0b6953dfff8a36b50e6ae75694e459497484ce7
Josh <josh.e.stein@gmail.com>
2024-06-04 10:46:50 -0400

    Extract darwin config into `darwin.nix`

--- darwin-flake.nix
+++ darwin-flake.nix
@@ -15,38 +15,11 @@
     };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ pkgs.vim
-        ];
-
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 4;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-    };
-  in
-  {
+  outputs = inputs@{ self, nix-darwin, nixpkgs }: {
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#joshstein-mac
     darwinConfigurations."joshstein-mac" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+      modules = [ ./darwin.nix ];
     };
 
     # Expose the package set, including overlays, for convenience.
---
+++ darwin.nix
@@ -0,0 +1,33 @@
+{ config, pkgs, ... }: {
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  # environment.systemPackages =
+  #   [ pkgs.vim
+  #   ];
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+  # nix.package = pkgs.nix;
+
+  # Necessary for using flakes on this system.
+  nix.settings.experimental-features = "nix-command flakes";
+
+  # Set Git commit hash for darwin-version.
+  system.configurationRevision = self.rev or self.dirtyRev or null;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+
+  # The platform the configuration will be used on.
+  nixpkgs.hostPlatform = "aarch64-darwin";
+
+  system.defaults = {
+    dock = {
+      autohide = true;
+      orientation = "left";
+      show-recents = false;
+    };
+  };
+};
+
