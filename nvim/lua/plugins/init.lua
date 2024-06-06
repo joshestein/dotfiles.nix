1f349466be86d409b916bd6abca01d47943dee89
Josh <josh.e.stein@gmail.com>
2024-06-06 02:27:43 -0400

    Remove smooth scroll

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -3,7 +3,6 @@ return {
   'HiPhish/rainbow-delimiters.nvim', -- Rainbow parentheses
   'github/copilot.vim',
   'justinmk/vim-sneak',
-  'karb94/neoscroll.nvim', -- Smooth scrolling
   'tpope/vim-repeat',
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   'tpope/vim-surround',
