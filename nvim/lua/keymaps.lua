b50970849a6eca563fcf0beb9da3f8a44fd869d9
Josh <josh.e.stein@gmail.com>
2024-08-09 12:51:06 -0400

    Cmd-backspace delete previous word

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -23,6 +23,9 @@ vim.keymap.set('n', '<leader>bn', '<cmd>bnext<CR>', { desc = 'Go to [N]ext buffe
 vim.keymap.set('n', '<leader>vs', '<cmd>vsplit<CR>', { desc = 'Open new vertical split' })
 vim.keymap.set('n', '<leader>hs', '<cmd>split<CR>', { desc = 'Open new horizontal split' })
 
+-- Make cmd-backspace delete the last word
+vim.keymap.set('i', '<D-BS>', '<C-w>')
+
 -- Terminal hotkey
 vim.keymap.set('n', '<C-\\>', '<cmd>tab terminal<CR>')
 vim.keymap.set('n', '<D-\\>', '<cmd>tab terminal<CR>')
