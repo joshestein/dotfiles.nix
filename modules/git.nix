ae8d5961819373deec78d02e2948f15932f16a88
Josh <josh.e.stein@gmail.com>
2024-11-18 00:41:22 +0200

    Fix git credentials

--- modules/git.nix
+++ modules/git.nix
@@ -1,3 +1,4 @@
+{ pkgs, ... }:
 {
   programs.git = {
     enable = true;
@@ -8,6 +9,9 @@
       init = { defaultBranch = "main"; };
       pull = { ff = "only"; };
       push = { autoSetupRemote = "true"; };
+      credential.helper = "${
+          pkgs.git.override { withLibsecret = true; }
+        }/bin/git-credential-libsecret";
     };
     delta = { enable = true; };
     aliases = {
