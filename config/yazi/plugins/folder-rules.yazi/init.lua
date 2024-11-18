b0ab93c386001b3b6fc9c7ca3efc1c0d3bd0b23e
Josh <josh.e.stein@gmail.com>
2024-11-19 01:21:59 +0200

    Initial folder rules

---
+++ config/yazi/plugins/folder-rules.yazi/init.lua
@@ -0,0 +1,20 @@
+local function setup()
+	ps.sub("cd", function()
+		local cwd = cx.active.current.cwd
+
+		if cwd:name() == "/home/josh" then
+			-- Don't show hidden folders in root
+			ya.manager_emit("show_hidden", { false })
+		else
+			ya.manager_emit("show_hidden", { true })
+		end
+
+		if cwd:ends_with("Downloads") then
+			ya.manager_emit("sort", { "modified", reverse = true, dir_first = false })
+		else
+			ya.manager_emit("sort", { "alphabetical", reverse = false, dir_first = true })
+		end
+	end)
+end
+
+return { setup = setup }
