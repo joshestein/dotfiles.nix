1d57cf7174e5f70969c3e9386f68ac8850ef0437
Josh Stein <josh.e.stein@gmail.com>
2025-08-16 10:46:48 +0200

    Yazi 'y' wrapper with cd to final dir

--- config/shell/functions.zsh
+++ config/shell/functions.zsh
@@ -115,3 +115,11 @@ lscat() {
     fi
   done
 }
+
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
+}
