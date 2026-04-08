581f36ea2be96183cbb496d8c16bdfd97db7169d
Josh Stein <josh.e.stein@gmail.com>
2026-04-08 23:48:09 +0200

    No need for `NVIM_LISTEN_ADDRESS` fixes

--- config/nvim/lua/plugins/neogit.lua
+++ config/nvim/lua/plugins/neogit.lua
@@ -21,19 +21,4 @@ return {
     },
   },
   cmd = 'Neogit',
-  config = function()
-    require('neogit').setup {}
-    -- Neovim 0.12+ automatically sets $NVIM_LISTEN_ADDRESS on startup.
-    -- When Neogit spawns a headless child Neovim as the git editor, the child
-    -- inherits this var and tries to bind to the same socket as the parent,
-    -- causing "address already in use" and a silent "commit failed" error.
-    -- Clearing it here prevents the child from inheriting the parent's socket.
-    local client = require 'neogit.client'
-    local original = client.get_envs_git_editor
-    client.get_envs_git_editor = function(show_diff)
-      local env = original(show_diff)
-      env.NVIM_LISTEN_ADDRESS = ''
-      return env
-    end
-  end,
 }
