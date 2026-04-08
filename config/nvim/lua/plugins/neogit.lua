53c7e031d5f1e285a9fcbc5282609e228917c46b
Josh Stein <josh.e.stein@gmail.com>
2026-04-09 00:03:39 +0200

    Eh fix NVIM_LISTEN_ADDRESS again

--- config/nvim/lua/plugins/neogit.lua
+++ config/nvim/lua/plugins/neogit.lua
@@ -9,16 +9,31 @@ return {
   keys = {
     { '<leader>gg', '<cmd>Neogit<cr>', desc = 'Neogit' },
   },
-  opts = {
-    mappings = {
-      popup = {
-        ['F'] = 'PullPopup',
-        ['p'] = 'PushPopup',
-      },
-      status = {
-        ['gz'] = 'RefreshBuffer',
-      },
-    },
-  },
   cmd = 'Neogit',
+  config = function()
+    require('neogit').setup {
+      mappings = {
+        popup = {
+          ['F'] = 'PullPopup',
+          ['p'] = 'PushPopup',
+          ['P'] = false,
+        },
+        status = {
+          ['gz'] = 'RefreshBuffer',
+        },
+      },
+    }
+
+    -- Neovim sets $NVIM_LISTEN_ADDRESS on startup. When Neogit spawns a
+    -- headless child nvim as the git editor, the child inherits this and
+    -- tries to bind to the same socket as the parent, causing "address
+    -- already in use" and a silent "commit failed". Clearing it here fixes this.
+    local client = require 'neogit.client'
+    local original = client.get_envs_git_editor
+    client.get_envs_git_editor = function(show_diff)
+      local env = original(show_diff)
+      env.NVIM_LISTEN_ADDRESS = ''
+      return env
+    end
+  end,
 }
